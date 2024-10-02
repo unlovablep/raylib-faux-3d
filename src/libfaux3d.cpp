2ac111567ca587b3738af7c921e0c088946b211e
Paige <unlovablep@gmail.com>
2024-10-02 00:01:38 +0000

    this is a cry for help

---
+++ src/libfaux3d.cpp
@@ -0,0 +1,18 @@
+#include <raylib.h>
+
+typedef struct
+{
+    int x;
+    int y;
+    int z;
+} Vec3;
+
+class
+Cube
+{
+    private:
+    Vec3 origin;    // origin point
+    Vec3 size;      // extends both directions from origin
+    public:
+};
+
--- src/main.c
+++
@@ -1,8 +0,0 @@
-#include <raylib.h>
-
-int
-main
-(int argc, char** argv)
-{
-    return 0;
-}
